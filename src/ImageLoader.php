756ba58ba4facb7afa4ba0394f1fd636b69a2ccd
Vadym Chenin <vchenin@meta.ua>
2016-10-23 14:24:22 +0300

    Code refactoring.

--- src/ImageLoader.php
+++ src/ImageLoader.php
@@ -15,7 +15,6 @@ class ImageLoader
     private $_destination;
     private $_supportedFormats = ['gif', 'jpg', 'png'];
 
-
     /**
      * Constructor.
      * 
@@ -38,7 +37,26 @@ class ImageLoader
         if (!$this->isSupportedFormat($format)) {
             array_push($this->_supportedFormats, $format);
         }
+
         return $this;
+    }
+
+    /**
+     * This method check accessibility of destination directory.
+     * 
+     * @return string Current path to destination directory.
+     * @throws \Exception
+     */
+    private function _checkDestinationDirectory()
+    {
+        $dirPath = $this->getDestination();
+
+        if ((!is_dir($dirPath) && !(@mkdir($dirPath))) || !is_writable($dirPath)) {
+            $msg = "Destination directory '$dirPath' is not accessible.";
+            throw new \Exception($msg);
+        }
+
+        return $dirPath;
     }
 
     /**
@@ -52,17 +70,66 @@ class ImageLoader
     }
     
     /**
+     * This method tries to get image content from given path.
+     * 
+     * @param string $imagePath Remote image path.
+     * 
+     * @return data Image raw data.
+     */
+    private function _getImageContent($imagePath)
+    {
+        $imageExt = pathinfo($imagePath, PATHINFO_EXTENSION);
+
+        if (!$this->isSupportedFormat($imageExt)) {
+            $msg = "File with extension '$imageExt' is not supported.";
+            $msg .= " Supported formats are: ";
+            $msg .= implode(', ', $this->_supportedFormats) . ".";
+            throw new \Exception($msg);
+        }
+
+        if (!$remoteImageContent = @file_get_contents($imagePath)) {
+            $msg = "Failed load image from path '$imagePath'.";
+            throw new \Exception($msg);
+        }
+
+        return $remoteImageContent;
+    }
+    
+     /**
+     * This method check possibility of using remote file name in that directory.
+     * 
+     * @param string $imagePath Remote image path.
+     * 
+     * @return string Local path for new image file.
+     * @throws \Exception
+     */
+    private function _getNewImagePath($imagePath)
+    {
+        $dirPath = $this->_checkDestinationDirectory();
+        $imageName = pathinfo($imagePath, PATHINFO_BASENAME);
+        $localImagePath = $dirPath . DIRECTORY_SEPARATOR . $imageName;
+
+        if (file_exists($localImagePath)) {
+            $msg = "File with the same name '$imageName' already exists";
+            $msg .= " in destination directory '$dirPath'.";
+            throw new \Exception($msg);
+        }
+
+        return $localImagePath;
+    }
+
+    /**
      * Generator which provide supported formats in reverse order from list.
      * 
-     * @return Iterator Description
+     * @return Iterator [$key (position in list) => $value (image extension)].
      */
-    public function getFormats()
+    public function getSupportedFormats()
     {
         for ($i = count($this->_supportedFormats) - 1; $i >= 0; $i--) {
             yield $i => $this->_supportedFormats[$i];
         }
     }
-    
+
     /**
      * This method check image format supporting.
      * 
@@ -81,41 +148,17 @@ class ImageLoader
      * 
      * @param string $imagePath Path to image source.
      * 
-     * @return ImageLoader Current instance.
+     * @return string Path to saved image.
      * @throws \Exception
      */
     public function load($imagePath)
     {
-        $imageExt = pathinfo($imagePath, PATHINFO_EXTENSION);
-        if (!$this->isSupportedFormat($imageExt)) {
-            $msg = "File with extension '$imageExt' is not supported.";
-            $msg .= " Supported formats are: ";
-            $msg .= implode(', ', $this->_supportedFormats) . ".";
-            throw new \Exception($msg);
-        }
+        $imageContent = $this->_getImageContent($imagePath);
+        $localImagePath = $this->_getNewImagePath($imagePath);
 
-        $remoteImageContent = file_get_contents($imagePath);
-        if (!$remoteImageContent) {
-            $msg = "Failed load image from path ($imagePath).";
-            throw new \Exception($msg);
-        }
+        file_put_contents($localImagePath, $imageContent);
 
-        $dirPath = $this->_destination;
-        if ((!is_dir($dirPath) && !(mkdir($dirPath))) || !is_writable($dirPath)) {
-            $msg = "Destination directory '$dirPath' is not accessible.";
-            throw new \Exception($msg);
-        }
-
-        $imageName = pathinfo($imagePath, PATHINFO_BASENAME);
-        $localImagePath = $dirPath . DIRECTORY_SEPARATOR . $imageName;
-        if (file_exists($localImagePath)) {
-            $msg = "File with the same name '$imageName' already exists";
-            $msg .= " in destination directory '$dirPath'.";
-            throw new \Exception($msg);
-        }
-        file_put_contents($localImagePath, $remoteImageContent);
-
-        return $this;
+        return $localImagePath;
     }
 
     /**
@@ -127,11 +170,12 @@ class ImageLoader
      */
     public function removeFormat($format)
     {
-        foreach ($this->getFormats() as $key => $value) {
+        foreach ($this->getSupportedFormats() as $key => $value) {
             if ($value == $format) {
                 unset($this->_supportedFormats[$key]);
             }
         }
+
         return $this;
     }
 
